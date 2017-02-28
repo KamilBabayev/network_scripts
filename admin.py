12dcc0a67313a1f9bd33e05e2c714387c13cd89c
kamil babayev <kamilyunis@gmail.com>
2017-02-28 04:43:42 +0400

    dev new func added

--- admin.py
+++ admin.py
@@ -1,5 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import os
 import sys
 
 os.system('ls /tmp')
+
+count=0
+while count < 10:
+    print(count)
+    count = count + 1
+
+def demo():
+    count=0
+    while count < 10:
+      count = count + 1
+      print(count)
+
+demo()
